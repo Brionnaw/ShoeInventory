21b02d1ac60670b6761ee49414a710e3928b8517
Brionna White <Brionnanwhite@gmail.com>
2016-08-25 15:47:26 -0700

    final

--- ngApp/js/controllers.ts
+++ ngApp/js/controllers.ts
@@ -1,14 +1,23 @@
 namespace app.Controllers {
   export class HomeController {
       public brandName;
+      public foundItem;
+      public image;
+      public response;
     constructor(private shoeService:app.Services.ShoeService) {
     }
     public search(){
       let brand = this.brandName;
       let data = brand.toLowerCase();
-      this.shoeService.checkInventory(data);
+      this.foundItem = this.shoeService.checkInventory(data);
+      if(this.foundItem === undefined){
+        this.response = "not in stock";
+    } else {
+      console.log(this.foundItem);
+        let temp = <HTMLImageElement>document.getElementById('image');
+        // temp.src = this.foundItem.image;
+      }
     }
   }
-
   angular.module('app').controller('HomeController', HomeController);
 }
